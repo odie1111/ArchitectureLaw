8569049c5439a692c360603964c800200688cc1a
AWAlphaMonkey <r97543050@ntu.edu.tw>
2017-07-06 21:24:56 +0800

    Add letter-modal prototype

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ var app = new Vue({
             this.showModal = true;
 
             // testing content
-            var test_content = {"law": "第一章 第 1 條 第 3 款", "letters": [{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "content": ["主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""}, {"entries": "內政部函 68. 04. 12. 台內營字第 002612 號", "content": ["主旨：核釋既成巷路（註一）上空可否加建一公尺（註二）寬之陽台疑義，請查照。", "說明：", "一、復貴局 68. 02. 27. 北市工建字第 61562 號函。", "二、按既成巷路為供公眾通行道路之一，具有公共地役關係存在，為維護公益自不得於其上空加建陽台以妨害交通。", "※註一：本函主自及說明二中之「既成巷路」，現行建集技術規則建築設計施工編第 1 條第 36 款已修正為「現有巷道」。", "※註二：建築技術規則原規定陽台應自外緣扣除「一公尺」後計算建築面積，現行條文已修正為「二點零公尺」。"], "img": "", "table": ""}, {"entries": "內政部函 70. 01. 31. 台內營字第 000886 號", "content": ["（刪除）"],"img": "","table": ""}, {"entries": "第三條之一", "content": [""]}, {"entries": "內政部函 73. 04. 03. 台內營字第 218267 號", "content": [""], "img": "", "table": ""}]};
+            var test_content = {"law": "第一章 第 1 條 第 3 款", "letters": [{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["1 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""}, {"entries": "內政部函 68. 04. 12. 台內營字第 002612 號", "contents": ["2 主旨：核釋既成巷路（註一）上空可否加建一公尺（註二）寬之陽台疑義，請查照。", "說明：", "一、復貴局 68. 02. 27. 北市工建字第 61562 號函。", "二、按既成巷路為供公眾通行道路之一，具有公共地役關係存在，為維護公益自不得於其上空加建陽台以妨害交通。", "※註一：本函主自及說明二中之「既成巷路」，現行建集技術規則建築設計施工編第 1 條第 36 款已修正為「現有巷道」。", "※註二：建築技術規則原規定陽台應自外緣扣除「一公尺」後計算建築面積，現行條文已修正為「二點零公尺」。"], "img": "", "table": ""}, {"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["3 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""},{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["4 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""},{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["5 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""},{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["6 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""},{"entries": "內政部函 67. 05. 31. 台內營字第 789796 號", "contents": ["7 主旨：建築物陽台外緣有結構樑時（如圖示），仍得依建築技術規則建築設計施工編第一條第三款之規定，自其外緣起扣除一公尺（註）後計算建築面積。請  查照。", "說明：復 67. 05. 15. 建四字第 71027 號函。", "※註：本函中「一公尺」，現行建築技術規則建築設計施工編第一條第三款已修正為「二點零公尺」"], "img": "", "table": ""}]};
             app.letter_item = test_content;
             console.log(test_content);
 
@@ -42,11 +42,11 @@ var app = new Vue({
                                     <slot name="header"></slot>
                                 </div>
                                 <div class="row entries modal-body">
-                                    <div class="col-xs-12 col-sm-3">
+                                    <div class="col-xs-12 col-sm-3 letter-box">
                                         <slot name="entries"></slot>
                                     </div>
-                                    <div class="col-xs-12 col-sm-9">
-                                        <slot name="content"></slot>
+                                    <div class="col-xs-12 col-sm-9 letter-box">
+                                        <slot name="contents"></slot>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
@@ -62,6 +62,41 @@ var app = new Vue({
             methods: {
                 close: function() {
                     this.$emit('close');
+                }
+            }
+        },
+        'zap-slideout': {
+            template: `
+                <div class="zap-slideout" :class="{ isOpen: isOpen }">
+                    <div class="zap-slideout-opener" @click="toggle">{{openerText}}</div>
+                        <ul class="zap-slideout-menu">
+                            <li class="zap-slideout-menu-item" v-for="item in menu">{{item}}</li>
+                            <li class="zap-slideout-menu-item--small" v-for="item in smallMenu">{{item}}</li>
+                        </ul>
+                    </div>
+                </div>
+            `,
+            data: () => ({
+                openerText: 'Open',
+                isOpen: false,
+                menu: [ 'Home', 'Work', 'Contact' ],
+                smallMenu: [ 'Tips', 'Resources', 'Shenanigans' ]
+            }),
+            methods: {
+                open() {
+                    this.openerText = 'Close';
+                    this.isOpen = true;
+                },
+                close() {
+                    this.openerText = 'Open';
+                    this.isOpen = false;
+                },
+                toggle() {
+                    if (this.isOpen) {
+                        this.close();
+                    } else {
+                        this.open();
+                    }
                 }
             }
         }
